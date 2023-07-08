d9d4bd2937fa14a2c77243b36ebde78d69ec6b25
Nelson Nobre <technobre@gmail.com>
2023-07-08 20:02:03 +0100

    fix: Package has been discontinued

--- src/Attributes/JWTAuthentication.cs
+++ src/Attributes/JWTAuthentication.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+﻿using System;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 
 namespace PowerUtils.AspNetCore.Authentication.JwtBearer.Attributes
 {
+    [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary. The package will be completely removed after 2024/01/08.")]
     public class JWTAuthentication : AuthorizeAttribute
     {
         public const string AUTHENTICATION_SCHEMES = JwtBearerDefaults.AuthenticationScheme;
