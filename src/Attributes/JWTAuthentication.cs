5ad96266c48c0ebf489bde1869b8612627c385d9
Nelson Nobre <technobre@gmail.com>
2023-07-08 20:23:45 +0100

    fix: force-deploy

--- src/Attributes/JWTAuthentication.cs
+++ src/Attributes/JWTAuthentication.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 
+
 namespace PowerUtils.AspNetCore.Authentication.JwtBearer.Attributes
 {
     [Obsolete("This package has been discontinued because it never evolved, and the code present in this package does not justify its continuation. It is preferable to implement this code directly in the project if necessary. The package will be completely removed after 2024/01/08.")]
